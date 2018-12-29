6c62d05f59c921fbe62be81188ef719678ffab36
Nikki Keister <nicolerkeister@gmail.com>
2018-12-28 21:16:31 -0500

    Create header and contact links

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import './styles/App.scss';
+import brushstroke from './resources/images/brushstroke.svg';
+
+
 
 class App extends Component {
     render() {
         const inText = <span className={'italic'} style={{margin: 1 + 'em'}}>in</span>;
         return (
             <div>
-                <header className={'title'}>nicole keister</header>
-                <p>UX DESIGNER {inText} NEW YORK</p>
+                <div className={'header'}>
+                    <img src={brushstroke} className={'brushstroke'} alt="brushstroke" />
+                    <header className={'title'}>Nicole Keister</header>
+                </div>
+                <div style={{marginTop: 2 + 'em'}}><a href={'https://linkedin.com/in/nicolekeister'}>linkedin</a></div>
+                <div style={{marginTop: 1 + 'em'}}><a href={'https://github.com/nrkeister/portfolio'}>github</a></div>
             </div>
         );
     }
